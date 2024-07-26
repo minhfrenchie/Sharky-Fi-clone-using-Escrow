362775a3004fb397c81fe9f72056fa8c50fee8ba
minhfrenchie <KalynZubek514@gmail.com>
2024-07-26 10:20:51 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("ANSFn4VzQ4WGuVMdadejGaD3LzeHETkeZWGaf6R63mV8");
 
 #[program]
 pub mod nft_lend_borrow {
